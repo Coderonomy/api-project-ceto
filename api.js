2c6fc12654bf3cda6dad795d1c299076b5d09bcb
BASHIR TOWDIEE <towdiee.bashir@gmail.com>
2019-01-25 05:31:57 +1100

    passport serialize, deserialize & cookie implemented

--- api.js
+++ api.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const mongoose = require('mongoose');
 const router = express.Router();
 const auth = require('./controllers/auth');
 const passportSetup = require('./config/passportSetup');
-
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 
 mongoose.connect('mongodb://localhost:27017/ceto',{ useNewUrlParser: true });
 mongoose.connection.on('connected', () => {
@@ -14,13 +15,29 @@ mongoose.connection.on('error', () => {
     console.log('failed to connect to mongod');
 });
 
+// mongodb: {
+//   dbURI: process.env.MONGODB_URI
+// }
+
+
+//initialize passport
+
 
 const app = new express();
-
 const port = process.env.PORT || 5000;
 
 //ejs 
 app.set('view engine', 'ejs')
+
+//encrypts cookie for 24h
+app.use(cookieSession({
+  maxAge: 24*60*60*1000,
+  keys: process.env.COOKIE_KEY
+}))
+
+// initialize passport
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 app.use(cors())
--- config/passportSetup.js
+++ config/passportSetup.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
+const User = require('../models/User')
 require('dotenv').config() 
+
+//stores user id inside cookie
+passport.serializeUser((user,done) => {
+    done(null,user.id);
+})
+
+//finds id from cookie returned from browser
+passport.deserializeUser((id,done) => {
+    User.findById(id).then((user) => {
+        done(null,id)
+    })
+})
 
 passport.use(
     new GoogleStrategy({
@@ -10,7 +23,28 @@ passport.use(
         clientSecret: process.env.GOOGLE_CLIENT_SECRET
 
     },(accessToken, refreshToken, profile, done)=>{
-        //passport callback function
-        console.log('passport callback function fired')
+        
+        //check if user already exists in our DB
+        User.findOne({googleId:profile.id}).then((currentUser) => {
+            if (currentUser){
+                //already have user
+                console.log(`already have user ${currentUser.username}`)
+                done(null,currentUser)
+            }else {
+                //create user in our DB
+                // console.log(profile)
+                new User({
+                    username: profile.name.familyName,
+                    googleId: profile.id,
+                    email: profile.emails[0].value,
+                    gender: profile.gender
+                }).save().then((newUser) => { 
+                    console.log(`new user created ${newUser}`)
+                    done(null,newUser)
+                })  
+            }
+        })
+
+        
     })
 )
