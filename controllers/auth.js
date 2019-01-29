ec93194b37094706808eb98bf9f8a40e32be8015
Luke Stainlay <lukestainlay@gmail.com>
2019-01-29 16:06:30 +1100

    updating data.json file

--- controllers/auth.js
+++ controllers/auth.js
@@ -22,11 +22,14 @@ router.get('/google', passport.authenticate('google',{
 }) )
 
 //callback routes for google to redirect to
-// check if user is in DB - handled in passportSetup.js
+// check if user is in DB - handled in passportSetup.js for the Google credentials only
+
 router.get('/google/redirect',passport.authenticate('google'), (req, res) => {
     // if the user has these fields completed (whether or not they were already a user) direct to appropriate path
     // need to build logic to return falsey value for all fields that are incomplete
     // need to work out those fields that we request completed
+    // also need a route to direct back to full sign up page if user has signed in with google previously (and therefore is already a user)
+    // console.log("console log user here:", req.user)
     if (req.user.myBusinessSkills[0]) {
         res.redirect('/protected/completed-profile')
     } else {
